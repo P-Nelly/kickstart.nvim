e7c9a1224cd9b7602b67c6d75eaed0d878ab81c0
unknown <git.kept777@passmail.net>
2025-09-30 16:36:21 -0700

    add change blocks and cd remap
    add block

--- init.lua
+++ init.lua
@@ -102,7 +102,9 @@ vim.g.have_nerd_font = false
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
+---------------------------------------------------------------------------------------
 vim.o.relativenumber = true
+---------------------------------------------------------------------------------------
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -114,9 +116,11 @@ vim.o.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
+---------------------------------------------------------------------------------------
 -- vim.schedule(function()
 --   vim.o.clipboard = 'unnamedplus'
 -- end)
+---------------------------------------------------------------------------------------
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -249,26 +253,28 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
+  ---------------------------------------------------------------------------------------
   {
-    "kdheepak/lazygit.nvim",
+    'kdheepak/lazygit.nvim',
     lazy = true,
     cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
     },
     -- optional for floating window border decoration
     dependencies = {
-      "nvim-lua/plenary.nvim",
+      'nvim-lua/plenary.nvim',
     },
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
   },
+  ---------------------------------------------------------------------------------------
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -902,7 +908,9 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    ---------------------------------------------------------------------------------------
     { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+    ---------------------------------------------------------------------------------------
   },
 
   -- Highlight todo, notes, etc in comments
@@ -1021,4 +1029,7 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+---------------------------------------------------------------------------------------
 vim.cmd.colorscheme 'catppuccin'
+vim.api.nvim_set_keymap('n', '<leader>cd', ':cd %:h<CR>', { noremap = true, silent = true })
+---------------------------------------------------------------------------------------
